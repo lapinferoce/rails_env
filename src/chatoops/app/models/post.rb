fcc66dc50d5d7cabf6bc2d8fdab68d501f4f1deb
root <root@localhost.localdomain>
2016-05-23 15:37:05 +0200

    fix

--- src/chatoops/app/models/post.rb
+++
@@ -1,6 +0,0 @@
-class Post < ActiveRecord::Base
-  validates :attachment,presence: true
-
-  has_attached_file :attachment, styles: { :medium => "640x" }
-  validates_attachment_content_type :attachment, :content_type => /\Aimage\/.*\Z/
-end
